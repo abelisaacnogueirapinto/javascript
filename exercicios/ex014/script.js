1ab16e571a5f1c971ac7d08424454d0aeb68b81d
Abel Isaac Nogueira Pinto <111458648+abelisaacnogueirapinto@users.noreply.github.com>
2022-09-14 16:30:50 -0300

    atual

--- exercicios/ex014/script.js
+++ exercicios/ex014/script.js
@@ -1,19 +1,28 @@
 function contar() {
-    let vinic = document.getElementById('inic')
-    let vfim= document.getElementById('fim')
-    let vpasso=document.getElementById('pas')
-    let res=document.querySelector('div#res')
-
-    if (vinic == "" || vfim =="" || vpasso =="" ){
-        alert('Início, fim e passo  não podem estar vazios ')
-    }else if (vinic >= vfim){
-        alert('O início deve ser menor do que o Fim')
-        res.innerHTML = `valor do inicio ${vinic} valor fim ${vfim}`
-    } else  if (vpasso=0) {
-          res.innerHTML = `Passo não pode ser zero`
-    }else{
-        res.innerHTML= `Contando:`
-    }
-    
-   
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+    if (ini.Value.length == 0 || fim.Value.length == 0 || passo.Value.length ==0 ) {
+        window.alert('Início fim e passo  não podem estar vazios ')    
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('passo invalido, considero 1')
+            p = 1
+        }
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F601}`
+             }
+        } else  {
+            for ( let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F601}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+   }
 }
