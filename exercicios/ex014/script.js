30b74665db9d2bf05a151395159a56d9b1757eed
Abel Isaac Nogueira Pinto <111458648+abelisaacnogueirapinto@users.noreply.github.com>
2022-09-15 00:25:26 -0300

    tabuada

--- exercicios/ex014/script.js
+++ exercicios/ex014/script.js
@@ -1,28 +1,32 @@
-function contar() {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
-    if (ini.Value.length == 0 || fim.Value.length == 0 || passo.Value.length ==0 ) {
-        window.alert('Início fim e passo  não podem estar vazios ')    
-    } else {
-        res.innerHTML = 'Contando: '
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)
-        if (p <= 0){
-            window.alert('passo invalido, considero 1')
-            p = 1
-        }
-        if (i < f) {
-            for (let c = i; c <= f; c += p) {
-                res.innerHTML += ` ${c} \u{1F601}`
-             }
-        } else  {
-            for ( let c = i; c >= f; c -= p) {
-                res.innerHTML += ` ${c} \u{1F601}`
-            }
-        }
-        res.innerHTML += `\u{1F3C1}`
-   }
+function contar(numero) {
+  console.log("aa");
+  let ini = document.getElementById("txti");
+  let fim = document.getElementById("txtf");
+  let passo = document.getElementById("txtp");
+  let res = document.getElementById("res");
+
+  
+  if (!ini.value || !fim.value || !passo.value) {
+    window.alert("Início fim e passo  não podem estar vazios ");
+  } else {
+    res.innerHTML = "Contando: ";
+    let i = Number(ini.value);
+    let f = Number(fim.value);
+    let p = Number(passo.value);
+    if (p <= 0) {
+      window.alert("passo invalido, considero 1");
+      p = 1;
+    }
+    if (i < f) {
+      for (let c = i; c <= f; c += p) {
+        res.innerHTML += ` ${c} \u{25AB}`;
+      }
+    }
+    if (i > f) {
+      for (let c = i; c >= f; c -= p) {
+        res.innerHTML += ` ${c} \u{1F601}`;
+      }
+    }
+    res.innerHTML += `\u{1F3C1}`;
+  }
 }
--- exercicios/ex015/script.js
+++ exercicios/ex015/script.js
@@ -1,1 +1,21 @@
-alert('olá')
+function tabuada() {
+
+    let num = document.getElementById('itab');
+    let res = document.getElementById('res');
+    let n = Number(num.value);
+    let op = document.querySelector('input[name="opera"]:checked').value;
+    if (!num.value) {
+        window.alert('favor colocar um número entre 0 e 10');
+    } else {
+        
+        res.innerHTML = "";
+
+        /*for (let c=0; c<=10;c++ ) {
+            let t = n * c;
+            res.innerHTML += `<p> ${n} X ${c} = ${t} </p> `
+
+        }*/
+        alert(document.querySelector('input[name=opera]:checked').value)
+    }
+
+}
