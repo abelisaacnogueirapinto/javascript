73c1f1b8743c5faecc3ba5b66b6551a7e08ee906
Abel Isaac Nogueira Pinto <111458648+abelisaacnogueirapinto@users.noreply.github.com>
2022-09-15 22:40:35 -0300

    tabu

--- exercicios/ex015/script.js
+++ exercicios/ex015/script.js
@@ -2,20 +2,52 @@ function tabuada() {
 
     let num = document.getElementById('itab');
     let res = document.getElementById('res');
-    let n = Number(num.value);
-    let op = document.querySelector('input[name="opera"]:checked').value;
+    let n =num.value;
+    /*
+    let opA = document.getElementById("isoma").value;
+    let opS = document.getElementById("isubt").value;
+    let opM = document.getElementById("imult").value;
+    let opD = document.getElementById("idivi").value;
+    */
+
+    let operador =document.querySelector('input[name=opera]:checked').value
     if (!num.value) {
         window.alert('favor colocar um número entre 0 e 10');
     } else {
         
         res.innerHTML = "";
-
-        /*for (let c=0; c<=10;c++ ) {
-            let t = n * c;
-            res.innerHTML += `<p> ${n} X ${c} = ${t} </p> `
-
-        }*/
-        alert(document.querySelector('input[name=opera]:checked').value)
     }
 
+
+    if (operador==="A") {
+        res.innerHTML = " Adição ";
+        for (let c=0; c<=10;c++ ) {
+            let t = n + c
+            res.innerHTML += `<p> ${n} + ${c} = ${t} </p> `
+        } 
+        
+    }
+    if (operador==="S")  {
+        res.innerHTML = "Subtração" ; 
+        for (let c=0; c<=10;c++ ) {
+            let t = n - c
+            res.innerHTML += `<p> ${n} - ${c} = ${t} </p> `
+        }      
+    }
+    if (operador==="M")  {
+        res.innerHTML = "Multiplicação";  
+        for (let c=0; c<=10;c++ ) {
+            let t = n * c
+            res.innerHTML += `<p> ${n} X ${c} = ${t} </p> `
+        }    
+          
+    }
+    if (operador==="D") {        
+           res.innerHTML = "Divisão";
+           for (let c=0; c<=10;c++ ) {
+            let t = n / c
+            res.innerHTML += `<p> ${n} / ${c} = ${t} </p> `
+        }         
+    }
+        //alert(document.querySelector('input[name=opera]:checked').value)*/    }
 }
