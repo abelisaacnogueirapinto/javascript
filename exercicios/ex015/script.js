8cf8884bcb6d4314c6930f63e98fdd3a10216e38
Abel Isaac Nogueira Pinto <111458648+abelisaacnogueirapinto@users.noreply.github.com>
2022-09-16 22:49:23 -0300

    calculadora tabuada

--- exercicios/ex015/script.js
+++ exercicios/ex015/script.js
@@ -1,54 +1,64 @@
 function tabuada() {
+  console.log("chegou a1");
+  let num = document.getElementById("itab");
+  let res = document.getElementById("res");
+  let n = num.value;
 
-    let num = document.getElementById('itab');
-    let res = document.getElementById('res');
-    let n =num.value;
-    let vcalc= document.getElementById('icalc')
-    let v2 = vcalc.value;
-
-    /*
+  /*
     let opA = document.getElementById("isoma").value;
     let opS = document.getElementById("isubt").value;
     let opM = document.getElementById("imult").value;
     let opD = document.getElementById("idivi").value;
     */
 
-    let operador =document.querySelector('input[name=opera]:checked').value
-   
-    if (!num.value) {
-        window.alert('favor colocar um número entre 0 e 10');
-    } else {
-        
-        res.innerHTML = "";
+  if (!num.value) {
+    window.alert("favor colocar um número");
+    res.innerHTML = "";
+  } else {
+    res.innerHTML = " Tabuada Multiplicação";
+    for (let c = 0; c <= 10; c++) {
+      let t = n * c;
+      res.innerHTML += `<p> ${n} X ${c} = ${t} </p> `;
     }
-    if (!calc.value) {
-        res.innerHTML = " Tabuada Multiplicação";  
-        for (let c=0; c<=10;c++ ) {
-            let t = n * c
-            res.innerHTML += `<p> ${n} X ${c} = ${t} </p> `
-        }    
-
-    } else if (operador==="A") {
-            res.innerHTML = " Adição ";
-            let t = n + v2                     
-            res.innerHTML = `<p> O resultado(Soma) é ${t}</p>`
-    } else if (operador==="S")  {
-            res.innerHTML = "Subtração" ; 
-            let t = n - v2;
-            res.innerHTML = `<p> O resultado(subtração) é ${t}</p>`
-    } if (operador==="M")  {
-            res.innerHTML = "Multiplicação";  
-            let t = n * v2;
-            res.innerHTML = `<p> O resultado(Multiplicação)é ${t}</p>`
-                
-    } else if (operador==="D") {        
-            res.innerHTML = "Divisão";
-            let t = n / v2;
-            res.innerHTML = `<p> O resultado(divisão) é ${t}</p>`
-    } 
+  }
 }
 
+function calcular() {
+  let n = Number(document.getElementById("itab").value);
+  let res = document.getElementById("res");
 
+  let v2 = Number(document.getElementById("icalc").value);
 
-        //alert(document.querySelector('input[name=opera]:checked').value)*/    }
+  let operador = document.querySelector("input[name=opera]:checked").value;
 
+  res.innerHTML = "";
+  if (!v2) {
+    window.alert(" Favor inserir um numero");
+    res.innerHTML = "";
+  }
+
+  if (operador === "A") {
+    res.innerHTML = " Adição ";
+    let t = Number(n) + Number(v2);
+    res.innerHTML = `<p> O resultado(Soma) é ${t}</p>`;
+  } else if (operador === "S") {
+    res.innerHTML = "Subtração";
+    let t = n - v2;
+    res.innerHTML = `<p> O resultado(subtração) é ${t}</p>`;
+  }
+  if (operador === "M") {
+    res.innerHTML = "Multiplicação";
+    let t = n * v2;
+    res.innerHTML = `<p> O resultado(Multiplicação) é ${t}</p>`;
+  } else if (operador === "D") {
+    res.innerHTML = "Divisão";
+    let t = n / v2;
+    res.innerHTML = `<p> O resultado(divisão) é ${t}</p>`;
+  }
+}
+
+function limpar() {
+  let res = document.getElementById("res");
+  res.innerHTML = " ";
+}
+//alert(document.querySelector('input[name=opera]:checked').value)*/    }
